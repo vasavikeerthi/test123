6fe9def692f5079a4880eb7e1eb0304350cf519b
vasavikeerthi <kannavasavikeerthi@gmail.com>
2020-10-06 09:13:40 -0400

    fc

--- JdbcDemo/src/com/vk/JdbcSelectDemo.java
+++ JdbcDemo/src/com/vk/JdbcSelectDemo.java
@@ -4,6 +4,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+// making changes in the code
 public class JdbcSelectDemo {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
